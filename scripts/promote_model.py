c4446909bcf0e440f1d109e18352135fb5127916
Sahil Gupta <2021.sahil.gupta@ves.ac.in>
2025-06-01 17:51:37 +0530

    CI-run7

--- scripts/promote_model.py
+++ scripts/promote_model.py
@@ -16,8 +16,8 @@ def promote_model():
     os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
     dagshub_url = "https://dagshub.com"
-    repo_owner = "vikashdas770"
-    repo_name = "YT-Capstone-Project"
+    repo_owner = "sahilcreator07"
+    repo_name = "End-to-End-ML-Pipeline"
 
     # Set up MLflow tracking URI
     mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
